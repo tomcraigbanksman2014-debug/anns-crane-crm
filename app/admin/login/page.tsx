555cacb0d4ee18f95bcdc4303675dc61f38570d0
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-01 11:50:10 +0000

    Update page.tsx

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import ClientShell from "../../ClientShell";
+import { ClientShell } from "../../ClientShell";
 
 export default function AdminLoginPage() {
   const router = useRouter();
