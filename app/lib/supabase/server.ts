2576244aea586b93ae46eb1a174d2e6afbc53d33
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-02 17:20:07 +0000

    Update server.ts

--- app/lib/supabase/server.ts
+++ app/lib/supabase/server.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export function createClient() {
+export function createSupabaseServerClient() {
   const cookieStore = cookies();
 
   return createServerClient(
