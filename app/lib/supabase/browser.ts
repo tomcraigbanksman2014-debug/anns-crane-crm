fbeb755a5f0204ae904876d8e2aa4cdf6d4bfcab
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-05 19:16:52 +0000

    Update browser.ts

--- app/lib/supabase/browser.ts
+++ app/lib/supabase/browser.ts
@@ -1,8 +1,17 @@
 "use client";
+
 import { createBrowserClient } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+let client: SupabaseClient | null = null;
+
 export function createSupabaseBrowserClient() {
-  return createBrowserClient(
+  if (client) return client;
+
+  client = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+
+  return client;
 }
