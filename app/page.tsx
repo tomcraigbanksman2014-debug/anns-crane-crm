1961e98183239383059977339af4fbf964a44432
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-09 17:01:48 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import { redirect } from "next/navigation";
+import { createSupabaseServerClient } from "./lib/supabase/server";
 
-export default function Page() {
+export default async function Page() {
+  const supabase = createSupabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
   redirect("/login");
 }
