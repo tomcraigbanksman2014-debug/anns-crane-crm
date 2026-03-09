45906ab5aec63d9a692e048302220b1218f94f92
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-09 16:56:10 +0000

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,13 +13,13 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: "/icon-192.png",
         sizes: "192x192",
-        type: "image/png"
+        type: "image/png",
       },
       {
         src: "/icon-512.png",
         sizes: "512x512",
-        type: "image/png"
-      }
-    ]
+        type: "image/png",
+      },
+    ],
   };
 }
