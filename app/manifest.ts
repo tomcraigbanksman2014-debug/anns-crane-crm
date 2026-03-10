954e25c43cbe28e40252d4972b63c8ef850508f8
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-10 10:36:04 +0000

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,6 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "AnnS CRM",
     description: "AnnS Crane Hire CRM",
     start_url: "/login",
+    scope: "/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#2c6fa3",
@@ -13,13 +14,15 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
-        type: "image/png"
+        type: "image/png",
+        purpose: "any",
       },
       {
         src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
-        type: "image/png"
-      }
-    ]
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
   };
 }
