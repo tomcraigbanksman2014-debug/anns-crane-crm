7ef4e7aaac4c97291b36de4211f923ae065dee67
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-14 22:25:48 +0000

    Update page.tsx

--- app/operators/new/page.tsx
+++ app/operators/new/page.tsx
@@ -1,19 +1,5 @@
-import ClientShell from "../../ClientShell";
-import OperatorForm from "../OperatorForm";
+import { redirect } from "next/navigation";
 
 export default function NewOperatorPage() {
-  return (
-    <ClientShell>
-      <div style={{ width: "min(900px, 95vw)", margin: "0 auto" }}>
-        <div>
-          <h1 style={{ margin: 0, fontSize: 32 }}>Add Operator</h1>
-          <p style={{ marginTop: 6, opacity: 0.8 }}>
-            Create a new operator record inside the CRM.
-          </p>
-        </div>
-
-        <OperatorForm mode="create" />
-      </div>
-    </ClientShell>
-  );
+  redirect("/admin/users");
 }
