702bb49e40ac7d1846287fa046390473be4613c1
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-03-17 08:15:13 +0000

    Update route.ts

--- app/api/transport-jobs/[id]/route.ts
+++ app/api/transport-jobs/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { createSupabaseServerClient } from "../../../../lib/supabase/server";
+import { createSupabaseServerClient } from "../../../lib/supabase/server";
 
 export async function POST(
   req: Request,
