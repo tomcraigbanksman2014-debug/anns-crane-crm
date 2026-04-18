43be6ff9842c25da353edc50f157e7bfc81854b1
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-04-18 20:30:42 +0100

    Update CraneDocumentsManager.tsx

--- app/cranes/[id]/CraneDocumentsManager.tsx
+++ app/cranes/[id]/CraneDocumentsManager.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import AssetDocumentManager, { type AssetDocumentItem } from "../../components/AssetDocumentManager";
+import type { AssetProfileInput } from "../../lib/assetAppendixPresets";
 
 export default function CraneDocumentsManager({
   craneId,
+  assetProfile,
   initialDocuments,
 }: {
   craneId: string;
+  assetProfile?: AssetProfileInput | null;
   initialDocuments: AssetDocumentItem[];
 }) {
   return (
     <AssetDocumentManager
       assetLabel="Crane"
+      assetType="crane"
+      assetProfile={assetProfile}
       uploadUrl={`/api/cranes/${craneId}/documents/upload`}
       deleteUrlPrefix={`/api/cranes/${craneId}/documents`}
       initialDocuments={initialDocuments}
