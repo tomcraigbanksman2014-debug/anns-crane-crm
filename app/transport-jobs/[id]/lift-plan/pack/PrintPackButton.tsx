49d3fe6a8cd8b648323c4c9f6ffa9af2942720fe
tomcraigbanksman2014-debug <tomcraigbanksman2014@gmail.com>
2026-05-14 15:02:51 +0100

    Update PrintPackButton to use custom print function

--- app/transport-jobs/[id]/lift-plan/pack/PrintPackButton.tsx
+++ app/transport-jobs/[id]/lift-plan/pack/PrintPackButton.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-export default function PrintPackButton() {
+import { printWithDocumentTitle } from "../../../../lib/printDocumentTitle";
+
+export default function PrintPackButton({ printTitle }: { printTitle?: string }) {
   return (
     <button
       type="button"
-      onClick={() => window.print()}
+      onClick={() => printWithDocumentTitle(printTitle)}
       style={{
         padding: "10px 14px",
         borderRadius: 10,
